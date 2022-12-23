073eb3b453962fa83a5d9125625d9b4b7972ae23
ChangMinPark2 <98qkrckdals@naver.com>
2022-12-23 17:29:51 +0900

    first commit

---
+++ src/main/java/com/example/chikenbreastsite/ChikenBreastSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.chikenbreastsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChikenBreastSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChikenBreastSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chikenbreastsite/ChikenBreastSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chikenbreastsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChikenBreastSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
